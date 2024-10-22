3e5ff075b5862003a619562d45a730dff1c4881c
Vandi <Lev-person11@yandex.ru>
2024-10-22 11:06:12 +0300

    Just a commit

--- main.go
+++ main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func BraketOf(expression string) (string, error) {
+	indexOpen := strings.Index(expression, "(")
+	indexClose := strings.Index(expression, ")")
+
+	for indexOpen > indexClose {
+		if indexClose == -1 {
+			return "", fmt.Errorf("400 Скобка должна быть закрыта")
+		}
+		if indexClose < indexOpen {
+			// Выполнение выражения без скобок
+			return expression, nil
+		}
+		BraketOf(expression[:indexOpen])
+		indexOpen = strings.Index(expression, "(")
+		indexClose = strings.Index(expression, ")")
+
+	}
+
+	return expression, nil
+
+}
+
+func Calc(expression string) (float64, error) {
+	expression = strings.Replace(expression, " ", "", -1)
+	index := strings.Index(expression, "(")
+	for index != -1 {
+		BraketOf(expression[index:])
+	}
+	var return_float float64
+	return return_float, nil
+}
+func main() {
+	Calc("abcdefghigklmnop")
+}
